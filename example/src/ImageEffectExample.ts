5b90ad459beb693919ebf1fe1c0a08dcc6568157
René Drieënhuizen <rene@mediamonks.com>
2018-04-09 10:29:00 +0200

    Add second example for the ImageEffectRenderer

--- example/src/ImageEffectExample.ts
+++ example/src/ImageEffectExample.ts
@@ -1,12 +1,59 @@
 import { ImageEffectRenderer } from '../../src/';
 const wolfenstein = require('./shader/wolfenstein.glsl');
+const smoothstep = require('./shader/smoothstep.glsl');
 
 export default class ImageEffectExample {
 
   private renderer: ImageEffectRenderer;
+  private hasImage: boolean = false;
+
+  private currentFrame:number;
 
   constructor(private wrapper:HTMLElement) {
-    this.renderer = ImageEffectRenderer.createTemporary(this.wrapper, wolfenstein, true);
-    this.renderer.play();
+    this.hasImage = this.wrapper.hasAttribute('data-image');
+
+    this.renderer = ImageEffectRenderer.createTemporary(
+      this.wrapper,
+      this.hasImage ? smoothstep : wolfenstein,
+      !this.hasImage,
+    );
+
+    if (this.hasImage) {
+      this.loadImages().then(this.init.bind(this));
+    } else {
+      this.renderer.play();
+    }
+  }
+
+  private init(images: Array<HTMLImageElement>):void {
+  	this.renderer.addImage(images[0], 0);
+  	this.renderer.addImage(images[1], 1);
+
+  	this.drawCurrentFrame();
+
+  	const range = <HTMLInputElement>document.querySelector('.js-progress');
+  	range.addEventListener('input', () => {
+  	  this.currentFrame = parseInt(range.value, 10);
+  	  this.drawCurrentFrame();
+    });
+  }
+
+  private drawCurrentFrame():void {
+    this.renderer.setUniformFloat('delta', this.currentFrame / ImageEffectRenderer.MAX_ANIMATION_FRAMES);
+    this.renderer.draw();
+  }
+
+  private loadImages():Promise<any> {
+    return Promise.all(
+      ['example.jpg', 'transition.jpg'].map(fileName => this.loadImage(fileName)),
+    );
+  }
+
+  private loadImage(fileName: string):Promise<HTMLImageElement> {
+    return new Promise((resolve) => {
+      const img = new Image;
+      img.onload = () => resolve(img);
+      img.src = `../static/${fileName}`;
+    });
   }
 }
