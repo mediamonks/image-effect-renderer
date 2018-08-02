65fdbbdc920e07bccfd10845d109cce53ea7a53c
René Drieënhuizen <rene@mediamonks.com>
2018-08-02 08:34:44 +0200

    Add new glitch example

--- example/src/ImageEffectExample.ts
+++ example/src/ImageEffectExample.ts
@@ -2,25 +2,30 @@ import { ImageEffectRenderer } from '../../src/';
 import ImageLoader from './ImageLoader';
 const wolfenstein = require('./shader/wolfenstein.glsl');
 const smoothstep = require('./shader/smoothstep.glsl');
+const glitch = require('./shader/glitch.glsl');
 
 export default class ImageEffectExample {
 
   private renderer: ImageEffectRenderer;
   private hasImage: boolean = false;
+  private isInfinite: boolean = false;
 
   private currentFrame:number;
 
   constructor(private wrapper:HTMLElement) {
     this.hasImage = this.wrapper.hasAttribute('data-image');
+    this.isInfinite = this.wrapper.hasAttribute('data-infinite');
 
     this.renderer = ImageEffectRenderer.createTemporary(
       this.wrapper,
-      this.hasImage ? smoothstep : wolfenstein,
+      this.hasImage ? smoothstep : this.isInfinite ? glitch : wolfenstein,
       !this.hasImage,
     );
 
     if (this.hasImage) {
       ImageLoader.loadImages(['example.jpg', 'transition.jpg']).then(this.init.bind(this));
+    } else if (this.isInfinite) {
+      ImageLoader.loadImages(['example.jpg']).then(this.init.bind(this));
     } else {
       this.renderer.play();
     }
@@ -28,15 +33,20 @@ export default class ImageEffectExample {
 
   private init(images: Array<HTMLImageElement>):void {
   	this.renderer.addImage(images[0], 0);
-  	this.renderer.addImage(images[1], 1);
 
-  	this.drawCurrentFrame();
+  	if (images[1]) {
+      this.renderer.addImage(images[1], 1);
 
-  	const range = <HTMLInputElement>document.querySelector('.js-progress');
-  	range.addEventListener('input', () => {
-  	  this.currentFrame = parseInt(range.value, 10);
-  	  this.drawCurrentFrame();
-    });
+      this.drawCurrentFrame();
+
+      const range = <HTMLInputElement>document.querySelector('.js-progress');
+      range.addEventListener('input', () => {
+        this.currentFrame = parseInt(range.value, 10);
+        this.drawCurrentFrame();
+      });
+    } else {
+  	  this.renderer.play();
+    }
   }
 
   private drawCurrentFrame():void {
