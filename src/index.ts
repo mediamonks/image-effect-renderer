6917d54b8a7d274e0c1cdb9e3478029546c6b2de
Reinder Nijhoff <reinder@infi.nl>
2023-11-07 12:04:31 +0100

    export types in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { default as ImageEffectRenderer } from './lib/ImageEffectRenderer.js';
 export { RendererInstance } from './lib/RendererInstance.js';
 export { RendererBuffer } from './lib/RendererBuffer.js';
+export type {ImageOptions} from './lib/Renderer.js';
+export type {ImageEffectRendererOptions} from './lib/ImageEffectRenderer.js';
+
